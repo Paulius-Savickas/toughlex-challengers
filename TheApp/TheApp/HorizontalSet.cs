6b9614a3378102a16fd7106b9b2d747315704f25
Lauris321 <laurynas.meksraitis@toughlex.com>
2019-02-28 22:18:42 +0200

    Vertical simple.

--- TheApp/TheApp/HorizontalSet.cs
+++ TheApp/TheApp/HorizontalSet.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// HorizontalSet.GetVSlidesList(dataSet.Pictures.ToArray().ToList()); 
+/// </summary>
+
 namespace TheApp
 {
     class HorizontalSet
