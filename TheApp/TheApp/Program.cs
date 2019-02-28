4c6a06f2567384b909b3f7aacaf5a553a73848e7
Žygimantas Butkus <zygjys@gmail.com>
2019-02-28 19:28:14 +0200

    File reading.

--- TheApp/TheApp/Program.cs
+++ TheApp/TheApp/Program.cs
@@ -1,17 +1,49 @@
-using System;
+using System.IO;
 
 namespace TheApp
 {
     class Program
     {
+        public class DataSet
+        {
+            public int R;
+            public int C;
+            //minimum number of each ingredient in slice
+            public int L;
+            //maximum number of slice cells
+            public int H;
+            public int Hadjusted;
+
+            public string[] Data;
+        }
+
+        public static DataSet ReadData(string filename)
+        {
+            var dataSet = new DataSet();
+
+            using (var sr = new StreamReader(filename))
+            {
+                var line = sr.ReadLine();
+                var bits = line.Split(' ');
+                dataSet.R = int.Parse(bits[0]);
+                dataSet.C = int.Parse(bits[1]);
+                dataSet.L = int.Parse(bits[2]);
+                dataSet.H = int.Parse(bits[3]);
+
+                dataSet.Data = new string[dataSet.R];
+
+                for (var i = 0; i < dataSet.R; i++)
+                {
+                    // ReSharper disable once PossibleNullReferenceException
+                    dataSet.Data[i] = sr.ReadLine().Trim();
+                }
+
+                return dataSet;
+            }
+        }
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var dataSet = ReadData("a_example.in"); 
         }
     }
 }
