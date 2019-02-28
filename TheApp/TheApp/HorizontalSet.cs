58d92dd36f5da6a9c8040c0695580ba56369f254
Lauris321 <laurynas.meksraitis@toughlex.com>
2019-02-28 21:21:34 +0200

    HorizontalSet start

---
+++ TheApp/TheApp/HorizontalSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheApp
+{
+    class HorizontalSet
+    {
+        HorizontalSet()
+        {
+
+        }
+
+        public int gettagcount(hashset<string> tags1, hashset<string> tags2)
+        {
+            var union = tags1.union(tags2);
+            return union.count();
+        }
+    }
+}
